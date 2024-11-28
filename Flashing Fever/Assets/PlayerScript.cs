d0417b21ec19a82386cba80a6ea582aed3d17a6a
VlakamI <v.kamianets@gmail.com>
2024-11-28 00:03:04 +0000

    Camera and different object type checks for future
    Implemented a basic 2d camera
    
    Sorted out the game objects for now
    
    Made it possible for different objects parents to hold different child objects for future parkour use

--- Flashing Fever/Assets/PlayerScript.cs
+++ Flashing Fever/Assets/PlayerScript.cs
@@ -9,10 +9,11 @@ public class PlayerScript : MonoBehaviour
     public LayerMask groundLayer;
     Vector2 moveDirection = Vector2.zero;
 
-    private float speedMultiplier = 5;
+    private float speedMultiplier = 15;
     private float jumpMultiplier = 50;
     private float horizontal;
-
+    private string[] colourList = { "Neutral", "Red" };
+    private string[] fullColourList = { "Neutral", "Red", "Green", "Blue" };
     //private void OnEnable()
     //{
     //    playerControls.Enable();
@@ -40,18 +41,21 @@ public class PlayerScript : MonoBehaviour
     }
     private bool IsGrounded()
     {
-        Debug.Log("check if grounded");
-        return Physics2D.OverlapCircle(groundCheck.position, 0.6f, groundLayer);
+        //Debug.Log("check if grounded");
+        return Physics2D.OverlapCircle(groundCheck.position, 0.7f, groundLayer);
     }
     public void Jump(InputAction.CallbackContext context)
     {
-        Debug.Log("attempt to jump");
+        //Debug.Log("attempt to jump");
         if (context.performed && IsGrounded())
         {
-            Debug.Log("jumped");
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpMultiplier);
+            if (ColourCheck())
+            {
+                //Debug.Log("jumped");
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpMultiplier);
+            }
         }
-        if(context.canceled && rb.linearVelocity.y > 0f)
+        if (context.canceled && rb.linearVelocity.y > 0f)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
         }
@@ -59,5 +63,28 @@ public class PlayerScript : MonoBehaviour
     public void Move(InputAction.CallbackContext context)
     {
         horizontal = context.ReadValue<Vector2>().x;
+    }
+    private bool ColourCheck()
+    {
+        Collider2D[] col = Physics2D.OverlapCircleAll(groundCheck.position, 0.7f, groundLayer);
+        foreach (Collider2D e in col)
+        {
+            if (ColourMatch(e.gameObject.transform.parent.name))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    private bool ColourMatch(string inp)
+    {
+        foreach (string i in colourList)
+        {
+            if (i == inp)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
